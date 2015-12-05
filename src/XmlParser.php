ba23531c603fc02e448062284e86ecfc75b89d58
lichunqiang <light-li@hotmail.com>
2015-12-05 11:25:37 +0800

    cs fix

--- src/XmlParser.php
+++ src/XmlParser.php
@@ -1,15 +1,26 @@
-<?php namespace light\yii2;
+<?php
+
+/*
+ * This file is part of the light/package.
+ *
+ * (c) lichunqiang <light-li@hotmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace light\yii2;
 
 use yii\base\Object;
 use yii\web\BadRequestHttpException;
 use yii\web\RequestParserInterface;
 
 /**
- * The request parser for xml
+ * The request parser for xml.
  *
- * @package light\yii2
  * @author lichunqiang <light-li@hotmail.com>
  * @license MIT
+ *
  * @version 0.2.11
  */
 class XmlParser extends Object implements RequestParserInterface
@@ -17,17 +28,19 @@ class XmlParser extends Object implements RequestParserInterface
     /**
      * If parser result as array, this is default,
      * if you want to get object, set it to false.
-     * @var boolean
+     *
+     * @var bool
      */
     public $asArray = true;
     /**
      * Whether throw the [[BadRequestHttpException]] if the process error.
-     * @var boolean
+     *
+     * @var bool
      */
     public $throwException = true;
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function parse($rawBody, $contentType)
     {
@@ -38,16 +51,17 @@ class XmlParser extends Object implements RequestParserInterface
         if ($result === false) {
             $errors = libxml_get_errors();
             $latestError = array_pop($errors);
-            $error = array(
+            $error = [
                 'message' => $latestError->message,
                 'type' => $latestError->level,
                 'code' => $latestError->code,
                 'file' => $latestError->file,
                 'line' => $latestError->line,
-            );
+            ];
             if ($this->throwException) {
                 throw new BadRequestHttpException($latestError->message);
             }
+
             return $error;
         }
 
